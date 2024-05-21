efc1fa73a90bcdc2dc4c39c9bf4bdd8ad17779ee
elnurqafarzade <morganofficial545@gmail.com>
2024-05-21 10:19:17 +0400

    Add files via upload

---
+++ 210524_task/Program.cs
@@ -0,0 +1,23 @@
+﻿#region Replace
+
+//string string1 = "Fenerbahce";
+//string string2 = string1.Replace('c', 'ç');
+// Console.WriteLine(string2); 
+
+#endregion
+
+#region Substring
+
+//string string1 = "Fenerbahce 1-0 Galatasaray";
+//string string2 = string1.Substring(0,10);
+//Console.WriteLine(string2);
+
+#endregion
+
+#region Trim
+
+//string string12 = "  Fenerbahce 1-0 Galatasaray  ";
+//string string2 = string12.Trim();
+//Console.WriteLine(string2);
+
+#endregion
